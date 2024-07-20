73cbfa802c3d2878d384f0d2c9d11838e3acb502
Duddukunta Divya <ddudduk1@in.ibm.com>
2024-07-20 10:55:09 +0530

    first commit

---
+++ nodejsproject/practice.js
@@ -0,0 +1,111 @@
+const example = ({ a, b, c }) => {
+    console.log(a, b, c);
+   };
+   example(0, 1, 2);
+
+var a = Math.max();
+var b = Math.min();
+console.log(a);
+console.log(b)
+// The Math.max() method returns -Infinity by default and the Math.min() 
+// method returns Infinity value by default when passed without any parameters
+
+
+const set = new Set();
+set.add(5);
+console.log("set",set)
+set.add('Hello');
+console.log("set2",set)
+set.add({ name: 'Scaler' });
+console.log("set3",set)
+for (let item of set) {
+ console.log(item + 6);
+}
+// First 2 numbers are added as integers, followed by string concatenation. 
+// Finally, since both are not of string type, JS 
+// stringifies both the object and the number and concatenates them. 
+// When an object is stringified it is read as [object Object] and then concatenated.
+
+var a = true + true + true * 3;
+console.log(a)
+// In Javascript, true is considered as the number 1 when used in any arithmetic expression,
+//  hence the expression evaluates to 5 and divideby, multiplication,substraction,addition.
+
+a = [1, 2, 3, 4, 5];
+console.log(a.slice(2, 4));
+// The slice() function in Javascript slices an array within the given start and end indexes and then returns the values lying in those ranges. 
+// The indexing done is 0-based indexing.
+
+var a = 1;  
+var b = 0;  
+while (a <= 3)  
+{  
+   a++;  
+   b += a * 2;  
+   console.log(b);
+}
+// The loop will run 3 times, before meeting the exit condition. 
+// First value of b will be 2 * 2 = 4, followed by 4 + 3 * 2 = 10, and then value of 10 + 4 * 2 = 18.
+
+var a = "Scaler";
+var result = a.substring(2, 4);
+console.log(result);
+// The substring function in javascript slices a substring out of a given string from the start to end indexes(excluding the end index). 
+// So the 2nd and 3rd characters are taken here(0-based indexing) and the answer is al.
+
+console.log(NaN === NaN);
+// In Javascript, NaN is not considered to be equal to NaN even after using the strict equality operator.
+
+const obj1 = {Name: "Hello", Age: 16};
+const obj2 = {Name: "Hello", Age: 16};
+console.log(obj1 === obj2);
+// The strict equality operator compares objects by their references so even though the contents of both objects are the same, 
+// their references don’t match resulting in false.
+
+// reverse the order of the string
+let revString = "Divya";
+let reversedString = revString =>revString.split('').reverse().join('');
+let results = reversedString(revString);
+console.log("reversedString",results)
+
+// Remove duplicates of array of elements
+let array1 =['1','1','3','4','6','7','9'];
+let removeDuplicate = [...new Set (array1)]
+console.log(removeDuplicate)
+
+// without using new Set and using index
+let uniqueArray = array1.filter((value,index,self) =>{
+    return self.indexOf(value) === index;
+});
+console.log("uniqueArray",uniqueArray);
+
+// using reduce how to remove duplicate values from array
+let uniqueArray2 = array1.reduce((accumulator,currentValue)  =>{
+    if(!accumulator.includes(currentValue)){
+        accumulator.push(currentValue);
+    }
+    return accumulator;
+},[])
+
+//  merge the 2 objects without overwriting existing properties?
+let object1 =  {"name":"Divya","language":"english"}
+let object2 = {"name":"Chinni","study":"same","languages":"telugu"}
+
+let object3 = {...object1,...object2}
+console.log("object3",object3);
+
+// Can you write a function in JavaScript to get the current date in the format “YYYY-MM-DD”?
+const currentDate = () =>{
+   return new Date().toISOString().split('T')[0];
+}
+console.log("currentDate",currentDate());
+
+// Can you write a function in JavaScript to calculate the cumulative sum of an array?
+const cumulativeSum = arr => arr.reduce((acc, num) => 
+[...acc, acc.length ? acc[acc.length - 1] + num : num], []);
+
+//  Can you write a function in JavaScript to split an array into chunks of a specified size?
+// Can you write a one-liner in JavaScript to find the longest consecutive sequence of a specific element in an array?
+const longestConsecutiveSequence = 
+(arr, element) => Math.max(...arr.join('').split(element).map(group => group.length));
+
